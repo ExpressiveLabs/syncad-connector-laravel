2e90cb09c1fc6d43df6837b057933b8be1e2bdce
Daniël Kamp <dwkiziko@gmail.com>
2019-03-25 13:38:21 +0100

    Updating aliases array.

--- src/mainstreamct/SyncadConnectorLaravel/config.php
+++ src/mainstreamct/SyncadConnectorLaravel/config.php
@@ -1,6 +1,11 @@
 <?php
   return [
+    // Contains the application's connection key
     'key' => env('SYNCAD_KEY'),
+
+    // Contains the application's display color
     'color' => env('SYNCAD_COLOR', ''),
+
+    // Contains the application's display name
     'name' => env('APP_NAME', 'Syncad Project'),
   ];

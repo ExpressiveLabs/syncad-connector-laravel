de4c0208de3a574360521ff879017b254dfedb7b
Daniël Kamp <dwkiziko@gmail.com>
2019-03-27 20:10:26 +0100

    Refactoring Routes.php

--- src/mainstreamct/SyncadConnectorLaravel/routes.php
+++ src/mainstreamct/SyncadConnectorLaravel/routes.php
@@ -4,10 +4,6 @@
 
   Route::group(['middleware' => ['web']], function () {
     Route::get('/ext/auth', function (Request $request) {
-      return redirect('/authenticator?key=' . $request->key);
-    });
-
-    Route::get('authenticator', function (Request $request) {
       return Syncad::authenticate($request->key, config('syncad.login_redirect'));
     });
   });
